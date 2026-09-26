e718dbf7aff9863d917f6132a314b0d8a9fe1bfe
phUR99 <skerrigen12@gmail.com>
2026-09-26 16:05:53 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -7,9 +7,6 @@ using namespace std;
 int solution(vector<vector<int>> jobs)
 {
     sort(jobs.begin(), jobs.end());
-
-    // {작업시간, 요청시간}
-    // priority_queue는 큰 값부터 나오므로 음수로 저장
     priority_queue<pair<int, int>> pq;
 
     int idx = 0;
@@ -18,7 +15,6 @@ int solution(vector<vector<int>> jobs)
 
     while (idx < jobs.size() || !pq.empty())
     {
-        // 현재 ed까지 요청된 작업을 모두 큐에 넣는다.
         while (idx < jobs.size() && jobs[idx][0] <= ed)
         {
             int t = jobs[idx][0];
@@ -28,15 +24,12 @@ int solution(vector<vector<int>> jobs)
             idx++;
         }
 
-        // 현재 처리할 작업이 없다면
-        // 다음 작업의 요청시간으로 이동
         if (pq.empty())
         {
             ed = jobs[idx][0];
             continue;
         }
 
-        // 작업시간이 가장 짧은 작업 하나 처리
         int d = -pq.top().first;
         int t = -pq.top().second;
         pq.pop();
